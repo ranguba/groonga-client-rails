8d3f8b195f5cc2626d51c6493cf85730e00f70e8
Kouhei Sutou <kou@clear-code.com>
2016-03-21 22:39:30 +0900

    Support the default Groonga client options by config/groonga_client.yml

---
+++ lib/groonga/client/railtie.rb
@@ -0,0 +1,60 @@
+# Copyright (C) 2016  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "groonga/client"
+
+module Groonga
+  class Client
+    # @private
+    class Railtie < ::Rails::Railtie
+      initializer "groonga-client.configure" do |app|
+        config_name = :groonga_client
+        config_path = Pathname(app.paths["config"].existent.first)
+        yaml_path = config_path + "#{config_name}.yml"
+        unless yaml_path.exist?
+          yaml_path.open("w") do |yaml|
+            yaml.puts(<<-YAML)
+default: &default
+  protocol: http
+  # protocol: https
+  host: 127.0.0.1
+  port: 10041
+  # user: alice
+  # password: secret
+  read_timeout: -1
+  # read_timeout: 3
+  backend: synchronous
+
+development:
+  <<: *default
+
+test:
+  <<: *default
+  port: 20041
+
+production:
+  <<: *default
+  host: 127.0.0.1
+  read_timeout: 10
+            YAML
+          end
+        end
+        Groonga::Client.default_options =
+          app.config_for(:groonga_client).deep_symbolize_keys
+      end
+    end
+  end
+end
