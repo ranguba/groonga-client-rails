f981e1194a16b3e557e3df7169406cd2c6970cd4
Kouhei Sutou <kou@clear-code.com>
2016-03-22 18:40:59 +0900

    Support testing

---
+++ test/run-test.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2016  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "rbconfig"
+
+Dir.glob("#{__dir__}/fixtures/*") do |test_application|
+  Dir.chdir(test_application) do
+    system(RbConfig.ruby, "-S", "rake", "test", "TESTOPTS=#{ARGV.join(' ')}")
+  end
+end
